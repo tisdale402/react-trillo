e2f352c4dac92a4793df5feefd755c96118f59c9
alex.tisdale <tisdale402@gmail.com>
2021-06-12 18:48:02 -0500

    Adds notifications to user nav

---
+++ src/components/usernav/userNavNotification/userNavNotification.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../../styles/sass/style.scss';
+
+const UserNavNotification = () => (
+    <span className='user-nav__notification'></span>
+);
+
+export default UserNavNotification;
