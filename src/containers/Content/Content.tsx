6cc8013bdb8ecdb8d85ecf0de787f65e7bff83ba
alex.tisdale <tisdale402@gmail.com>
2021-06-13 20:26:57 -0500

    Adds sidebar layout

--- src/containers/Content/Content.tsx
+++ src/containers/Content/Content.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../styles/sass/style.scss';
 import HotelView from "../HotelView/HotelView";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Content = () => (
     <div className='content'>
+        <Sidebar/>
         <HotelView/>
     </div>
 );
---
+++ src/containers/Sidebar/Sidebar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../styles/sass/style.scss';
+
+const Sidebar = () => (
+    <div className='sidebar'></div>
+);
+
+export default Sidebar;
