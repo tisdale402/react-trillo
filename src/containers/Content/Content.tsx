dd2bd4ecf8ab75033982c66785ff6fd8dee069a3
alex.tisdale <tisdale402@gmail.com>
2021-06-19 09:13:16 -0500

    Add detail and description sections

--- src/containers/Content/Content.tsx
+++ src/containers/Content/Content.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../styles/sass/style.scss';
 import HotelView from "../HotelView/HotelView";
 import Sidebar from "../Sidebar/Sidebar";
-import Overview from "../Overview/Overview";
 
 const Content = () => (
     <div className='content'>
