bbfb4ed312dae52803af324f12aed2c8f59fe264
alex.tisdale <tisdale402@gmail.com>
2021-06-19 21:10:37 -0500

    Adds CTA text

--- src/containers/CTA/CTA.tsx
+++ src/containers/CTA/CTA.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import '../../styles/sass/style.scss';
 
 const CTA = () => (
-    <div className='cta'></div>
+    <div className='cta'>
+        <h2 className='cta__book-now'>Good News! We Have 4 Free Rooms For Your Selected Dates</h2>
+    </div>
 );
 
 export default CTA;
