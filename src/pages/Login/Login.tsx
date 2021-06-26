1732feec9d70291cf8e06c8fb65985774e8c82d1
alex.tisdale <tisdale402@gmail.com>
2021-06-26 14:41:34 -0500

    Adds support for routing

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../styles/sass/style.scss';
+
+const Login = () => (
+    <React.Fragment>
+        <div>
+            Trillo
+        </div>
+        <div>
+            The All In One Trip Advisor
+        </div>
+        <button className='btn'>
+            Sign In
+        </button>
+    </React.Fragment>
+);
+
+export default Login;
