178425e8b930fc2dfa50341997b22b4ec03ba164
alex.tisdale <tisdale402@gmail.com>
2021-06-29 20:41:05 -0500

    Add login page

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -5,7 +5,7 @@ import Homepage from "../../pages/Homepage/Homepage";
 import Login from "../../pages/Login/Login";
 
 const App = () => (
-    <div className='App'>
+    <div>
         <Route path='/login'>
             <Login/>
         </Route>
